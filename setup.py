ac0ad9eaef5752581de381130a44b376975d3a0e
Guillem <guillem@calidae.com>
2020-05-22 20:11:05 +0200

    Factory trytond pypi publish (#7)
    Add github action to publish library to pypi on git tags
    
    Co-authored-by: GuillemSune97 <gsunemontana@gmail.com>

--- setup.py
+++ setup.py
@@ -9,8 +9,25 @@ README = (HERE / "README.md").read_text()
 setup(
     name="factory_trytond",
     version="0.1.0",
+    author="Calidae",
+    author_email="dev@calidae.com",
+    description="Factory Boy - Trytond integration",
     long_description=README,
     long_description_content_type="text/markdown",
+    url="https://github.com/calidae/factory-trytond",
     packages=find_packages("factory_trytond"),
-    install_requires=["factory_boy", "trytond"]
+    install_requires=["factory_boy", "trytond"],
+    classifiers=[
+        "Framework :: Tryton",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    license='GPL-3',
+    python_requires='>=3.5',
 )
