ec9ca7460dd617c14ba794a7ba5c01b342f58265
GuillemSune97 <gsunemontana@gmail.com>
2020-05-22 19:04:16 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -21,10 +21,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=[
-        '3.5',
-        '3.6',
-        '3.7',
-        '3.8',
-    ],
+    python_requires='>=3.5',
 )
