cf90529782f927a997901f2d2af1faf0d0a71edf
GuillemSune97 <gsunemontana@gmail.com>
2020-05-22 19:58:20 +0200

    Setup classifiers

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="factory_trytond",
-    version="0.1.1",
+    version="0.1.0",
     author="Calidae",
     author_email="dev@calidae.com",
     description="Factory Boy - Trytond integration",
@@ -18,9 +18,16 @@ setup(
     packages=find_packages("factory_trytond"),
     install_requires=["factory_boy", "trytond"],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Framework :: Tryton",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
+    license='GPL-3',
     python_requires='>=3.5',
 )
